d6b8939896251ee10dd599f15981599278ecad0c
kitce <kitce.dev@gmail.com>
2024-08-05 12:54:18 +0800

    feat: add web share

--- src/pages/PlayOnline/Waiting.tsx
+++ src/pages/PlayOnline/Waiting.tsx
@@ -1,4 +1,5 @@
 import { faCopy } from '@fortawesome/free-regular-svg-icons/faCopy';
+import { faArrowUpFromBracket } from '@fortawesome/free-solid-svg-icons/faArrowUpFromBracket';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import useClipboard from '@gtomato-web/react-hooks/lib/web/useClipboard';
 import {
@@ -10,7 +11,9 @@ import {
 } from '@nextui-org/react';
 import classNames from 'classnames';
 import React, { useCallback } from 'react';
+import { generatePath } from 'react-router-dom';
 
+import { ROUTES } from '../../constants/routes';
 // import styles from './Waiting.module.scss';
 
 /**
@@ -40,6 +43,15 @@ const Waiting: React.FunctionComponent<TProps> = props => {
     copy(id);
   }, [id, copy]);
 
+  const handleShareButtonClick = useCallback(async () => {
+    try {
+      const url = generatePath(ROUTES.PlayOnline, { id });
+      await window.navigator.share?.({ url });
+    } catch (err) {
+      // TODO error handling
+    }
+  }, [id]);
+
   return (
     <section
       className={classNames(className, 'flex flex-col items-center gap-4')}
@@ -50,21 +62,32 @@ const Waiting: React.FunctionComponent<TProps> = props => {
       </div>
       <div className='flex items-center gap-2'>
         {id}
-        <Popover placement='right'>
-          <PopoverTrigger>
-            <Button
-              isIconOnly
-              size='sm'
-              onClick={handleCopyButtonClick}
-              aria-label='Copy'
-            >
-              <FontAwesomeIcon icon={faCopy} />
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent>
-            <span>Copied</span>
-          </PopoverContent>
-        </Popover>
+        {window.navigator.share ? (
+          <Button
+            isIconOnly
+            size='sm'
+            onClick={handleShareButtonClick}
+            aria-label='Share'
+          >
+            <FontAwesomeIcon icon={faArrowUpFromBracket} />
+          </Button>
+        ) : (
+          <Popover placement='right'>
+            <PopoverTrigger>
+              <Button
+                isIconOnly
+                size='sm'
+                onClick={handleCopyButtonClick}
+                aria-label='Copy'
+              >
+                <FontAwesomeIcon icon={faCopy} />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent>
+              <span>Copied</span>
+            </PopoverContent>
+          </Popover>
+        )}
       </div>
     </section>
   );
